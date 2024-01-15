3619c2abf26a756226b9aacce7a7821491eb7a6d
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-15 21:04:24 +0530

    check

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function child(props) {
+
+    return (
+        <div>
+            <h1>Hey {props.name}!</h1>
+            <h2>Welcome to School.</h2>
+        </div>
+    )
+}
